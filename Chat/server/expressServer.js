ac817c0f1ff37f5f196ae839ef5521cbe1ccb2db
DMisilin <Milissa990@yandex.ru>
2019-06-11 16:54:38 +0300

    1

--- Chat/server/expressServer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/', function (req, res) {
-   res.sendfile(__dirname + '/Page.html');
-})
-app.listen(3000, function () {
-   console.log('Example app listening on port 3000!')
-})  
